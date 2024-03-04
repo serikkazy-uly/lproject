040e3d1cca9eef3e84d91e23e422946e7c70059e
serikkazy-uly <damirkap89@gmail.com>
2024-03-04 13:56:46 +0500

    feat: Add restore row from db

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,49 +9,76 @@ class UserController extends Controller
 {
     public function index()
     {
+        // dd($user);
         $users = User::all();
+        // dd($users);
         return view('user.index', ['users' => $users]);
     }
 
     public function create()
     {
-        return view('user.create');
+        $userArr = [
+            [
+                'name' => 'dayana2',
+                'email' => 'dayana2@email.com',
+                'password' => 'password2',
+            ],
+        ];
+
+        foreach ($userArr as $user) {
+            // dd($user);
+            User::create($user); // Добавил юзера
+        }
+        dd('created');
+        // return view('user.create');
     }
 
-    public function store(Request $request)
+    // public function store(Request $request)
+    // {
+    //     // dd($request);    
+    //     $data = $request->validate([
+    //         'name' => 'required',
+    //         'email' => 'required|email|unique:users',
+    //         'password' => 'required|min:6',
+    //     ]);
+    //     User::create($data);
+    //     return redirect(route('user.index'));
+    // }
+
+    // public function edit(User $user)
+    // {
+    //     return view('user.edit', ['user' => $user]);
+    // }
+
+    public function update(User $user)
     {
-        // dd($request);    
-        $data = $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:user',
-            'password' => 'required|min:6',
+        $user = User::find(7);
+        $user->update([
+            'name' => 'updated',
+            'email' => 'updated',
+            'password' => 'updated',
         ]);
-        User::create($request->all());
-        $newUser = User::create($data);
+        // dd('updated');
 
-        return redirect(route('user.index'));
+        // $data = $request->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|email|unique:users',
+        //     'password' => 'required|min:6',
+        // ]);
+        // $user->update($data);
+        // dd($user);
+        // return redirect(route('user.index'))->with('success', 'User Updated Succesffully');
     }
 
-    public function edit(User $user)
+    public function delete(User $user)
     {
-        return view('user.edit', ['user' => $user]);
-    }
-
-    public function update(User $user, Request $request)
-    {
-        // dd($request);    
-        $data = $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:user',
-            'password' => 'required|min:6',
-        ]);
-        $user->update($data);
-        return redirect(route('user.index'))->with('success', 'User Updated Succesffully');
-    }
-
-    public function destroy(User $user)
-    {
-        $user->delete();
-        return redirect(route('user.index'))->with('success', 'User deleted Succesffully');
-    }
+        // dd('delete page');
+        $user = User::withTrashed()->find(2);
+        // dd($user->name);
+        $user->restore();
+        // $user->delete();
+        dd('deleted');
+        // $user->delete();
+        // return redirect(route('user.index'))->with('success', 'User deleted Succesffully');
+    }   
 }
